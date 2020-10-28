c0d56ca81f7a11e1d367a64ecd5845f7d8a0005e
tuiu <13719283454@163.com>
2020-10-28 15:11:33 +0800

    新建登录组件

---
+++ src/components/login/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-container">
+
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator"
+
+  @Component
+  export default class Login extends Vue {
+    
+  }
+</script>
+<style scoped lang="stylus">
+
+</style>
